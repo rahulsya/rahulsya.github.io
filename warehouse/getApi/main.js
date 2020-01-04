71522dcee1ec18afba96ec981e67049c8592411b
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-01-04 20:53:57 +0700

    update script
    get api using async await

--- warehouse/getApi/main.js
+++ warehouse/getApi/main.js
@@ -1,54 +1,32 @@
-const app=document.getElementById('root')
 
-const logo=document.createElement('img')
-logo.src='image/logo.png'
+const container=document.querySelector(".container")
 
-const container=document.createElement('div')
-container.setAttribute('class','container')
-
-app.appendChild(logo)
-app.appendChild(container)
-
-//create a request var
-var request = new XMLHttpRequest()
-
-//get url
-request.open('GET','https://ghibliapi.herokuapp.com/films',true)
-
-request.onload=function(){
-    var data=JSON.parse(this.response)
-
-    if (request.status >=200 && request.status <=300) {   
-        data.forEach(movie => {
-            //console.log(movie.title);
-            //console.log(movie.description);
-
-            //create div with class card
-            const card=document.createElement('div')
-            card.setAttribute('class','card')
-
-            const h1=document.createElement('h1')
-            h1.textContent=movie.title
-
-            const p = document.createElement('p')
-            movie.description=movie.description.substring(0,300)//limit to 300 char
-            p.textContent=`${movie.description}...` //end with elips
-
-            //append card to container elemet
-            container.appendChild(card)
-
-            //append content to card element
-            card.appendChild(h1)
-            card.appendChild(p)
-            
-        }); 
-    }
-    else{
-        const errorMessage=document.createElement('h1')
-        errorMessage.textContent='404 not work'
-        app.appendChild(errorMessage)
-        console.log('error')
+async function getApi(){
+    try {
+        let response=await fetch('https://ghibliapi.herokuapp.com/films')
+        let result=await response.json()
+        return result
+    } catch (error) {
+        console.log('error');
     }
 }
-//send Request
-request.send()
+
+function display(movies){
+    let result=""
+    movies.forEach(movie => {
+        result+=`
+        <div class="card">
+            <h1>${movie.title}</h1>
+            <p>${movie.description.substring(0,300)}...</p>
+        </div>
+        `
+    });
+    container.innerHTML=result
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    getApi().then(data=>{
+        display(data)
+    })
+})
+
